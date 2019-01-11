a9c8213e1dfbd8bebf61289c244118e3b77dcba3
Thomas Madden <tmadden@mgh.harvard.edu>
2019-01-11 14:55:52 -0500

    Fix build.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     setup_requires=["pytest-runner", "setuptools-git-version"],
     install_requires=["numpy"],
     tests_require=["pytest", "scikit-learn>=0.20"],
-    extras_require={"sklearn": ["scikit-learn>=0.20"]}
+    extras_require={"sklearn": ["scikit-learn>=0.20"]},
     packages=["peptide"],
     package_dir={"": "src"},
     version="0",
